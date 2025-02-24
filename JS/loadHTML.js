ba0b7886951f48bf2df4dc42caabaf6e939d644e
Igor Krysin <74359847+igorkrisin@users.noreply.github.com>
2025-02-24 11:10:51 +0300

    Create loadHTML.js

---
+++ JS/loadHTML.js
@@ -0,0 +1,9 @@
+// loadHTML.js
+function loadHTML(elementId, file) {
+    fetch(file)
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById(elementId).innerHTML = data;
+        })
+        .catch(error => console.error('Ошибка при загрузке:', error));
+}
