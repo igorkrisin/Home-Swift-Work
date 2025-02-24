cf8eca30f00a15b4219f4e10d95c3b6aecebe8ae
Igor Krysin <74359847+igorkrisin@users.noreply.github.com>
2025-02-24 14:54:57 +0300

    Update header.js

--- JS/header.js
+++ JS/header.js
@@ -15,6 +15,6 @@ function loadHeader() {
     fetch('header.html')
         .then(response => response.text())
         .then(data => {
-            document.getElementById('header').innerHTML = data;
+            document.getElementById('header').innerHTML = data; // Исправлено на 'header'
         });
 }
