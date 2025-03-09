1a59f17a937c0415d7fcc50402c1b0edccb1cbbe
Igor Krysin <74359847+igorkrisin@users.noreply.github.com>
2025-03-09 22:23:04 +0300

    Update loadHTML.js

--- JS/loadHTML.js
+++ JS/loadHTML.js
@@ -1,14 +1,23 @@
-// loadHTML.js
+function getRelativePath(file) {
+    let depth = window.location.pathname.split('/').length - 2; // Количество вложенных папок
+    let prefix = '../'.repeat(depth); // Формируем путь к корню
+    return prefix + file; // Возвращаем корректный путь
+}
+
 function loadHTML(elementId, file) {
-    fetch(file)
-        .then(response => response.text())
+    let path = getRelativePath(file);
+    fetch(path)
+        .then(response => {
+            if (!response.ok) throw new Error(`Ошибка загрузки ${file}: ${response.statusText}`);
+            return response.text();
+        })
         .then(data => {
             document.getElementById(elementId).innerHTML = data;
         })
-        .catch(error => console.error('Ошибка при загрузке:', error));
+        .catch(error => console.error(error));
 }
 
-// Загрузка хедера и футера
+// Загружаем хедер и футер с учетом вложенности страницы
 window.onload = function() {
     loadHTML('header', 'header.html');
     loadHTML('footer', 'footer.html');
